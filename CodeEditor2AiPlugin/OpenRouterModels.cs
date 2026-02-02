65a6a2da058d67bc2deaef400a6ab0830704ebdd
ajkfds <ajkfds2018@gmail.com>
2026-02-03 08:04:05 +0900

    add openrouter models

--- CodeEditor2AiPlugin/OpenRouterModels.cs
+++ CodeEditor2AiPlugin/OpenRouterModels.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace pluginAi
 {
@@ -20,6 +21,11 @@ namespace pluginAi
         public static Model openai_gpt_4p1_nano             = new Model("openai/gpt-4.1-nano", "OpenAI: GPT-4.1 Nano", 1_047_576, 0.10, 0.40);// $10/K web search)
         public static Model xiaomi_mimo_v2_flash_free = new Model("xiaomi/mimo-v2-flash:free", "Xiaomi: MiMo-V2-Flash (free)", 262_144, 0, 0);
         public static Model anthropic_claude_3p7_sonnet = new Model("anthropic/claude-3.7-sonnet", "Anthropic: Claude 3.7 Sonnet", 200_000, 3, 15);
+        public static Model google_gemini_3_pro_preview = new Model("google/gemini-3-pro-preview", "Google: Gemini 3 Pro Preview", 1_048_576, 2, 12);
+        public static Model google_gemini_3_flash_preview = new Model("google/gemini-3-flash-preview","Google: Gemini 3 Flash Preview", 1_048_576,0.50,3);
+        public static Model deepseek_deepseek_v3_2 = new Model("deepseek/deepseek-v3.2", "DeepSeek: DeepSeek V3.2", 163_840, 0.25, 0.38);
+        public static Model openai_gpt_5_1_codex_mini = new Model("openai/gpt-5.1-codex-mini", "OpenAI: GPT-5.1-Codex-Mini", 400_000, 0.25, 2);
+
         public class Model
         {
             public Model(string name,string caption)
