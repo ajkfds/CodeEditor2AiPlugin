28e3a31c5468be983e9fa6c8f6d77188046be891
ajkfds <ajkfds2018@gmail.com>
2026-08-19 06:41:06 +0900

    fix refactor code

--- CodeEditor2AiPlugin/NavigatePanel/NavigatePanelMenu.cs
+++ CodeEditor2AiPlugin/NavigatePanel/NavigatePanelMenu.cs
@@ -79,7 +79,7 @@ namespace pluginAi.NavigatePanel
             if (!relativePath.EndsWith(System.IO.Path.DirectorySeparatorChar)) relativePath += System.IO.Path.DirectorySeparatorChar;
 
             CodeEditor2.Tools.InputWindow window = new CodeEditor2.Tools.InputWindow("Create new " + typeName, "new " + typeName + " name");
-            await Controller.ShowDialog(window);
+            await Controller.ShowDialogAsync(window);
 
             if (window.Cancel) return;
             string name = window.InputText.Trim();
